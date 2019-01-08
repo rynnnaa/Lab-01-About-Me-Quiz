1a96719a3fd3ccc45fbeb808b0167d60ef0819b4
Tulip Pink <pinktulips@hotmail.com>
2019-01-08 09:54:14 -0800

    adding questions

--- AboutMe/AboutMe/Program.cs
+++ AboutMe/AboutMe/Program.cs
@@ -7,6 +7,21 @@ namespace AboutMe
         static void Main(string[] args)
         {
             Console.WriteLine("How well do you know me?");
+
+            Console.Write("Do I have a pet parrot?");
+            string parrot = Console.ReadLine();
+
+            Console.WriteLine("Can I swim?");
+            string canISwim = Console.ReadLine();
+
+            Console.WriteLine("What is my favorite candy?");
+            string candy = Console.ReadLine();
+
+            Console.WriteLine("How many sisters do I have?");
+            string sister = Console.ReadLine();
+
+
         }
+
     }
 }
