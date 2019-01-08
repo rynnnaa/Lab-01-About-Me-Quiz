8f3a7cdebcadf8da320dddce4ba319b1ae88b4f9
Tulip Pink <pinktulips@hotmail.com>
2019-01-08 09:46:34 -0800

    add this

---
+++ AboutMe/AboutMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How well do you know me?");
+        }
+    }
+}
